199af49da96d47dc24382da6f388ef8b55716c91
Kanika <kanikachander1@gmail.com>
2018-07-10 14:48:06 +0530

    reverse-string-js-problem

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,11 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+const reverse = (str) => {
+  const array = str.split('');
+  const reversedArray = array.reverse();
+  let reversedString = reversedArray.toString();
+  reversedString = reversedString.replace(/,/g, '');
+  return reversedString;
+};
 
-module.exports = reverse;
+export default reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,10 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+const reverse = (a) => {
+  let reversedString = '';
+  for (let i = a.length - 1; i >= 0; i -= 1) {
+    reversedString += a[i];
+  }
+  return reversedString;
+};
 
 module.exports = reverse;

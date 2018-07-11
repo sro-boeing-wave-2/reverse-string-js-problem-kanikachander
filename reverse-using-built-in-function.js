93e298146c65bd353ed79258735c99a9d80ab096
Kanika <kanikachander1@gmail.com>
2018-07-11 17:11:05 +0530

    #1 Replaced .toString() and .replace() with .join(), used arror function to return statement in reverse-using-built-in-function.js

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,11 +1,5 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = (str) => {
-  const array = str.split('');
-  const reversedArray = array.reverse();
-  let reversedString = reversedArray.toString();
-  reversedString = reversedString.replace(/,/g, '');
-  return reversedString;
-};
+const reverse = str => str.split('').reverse().join('');
 
 module.exports = reverse;
